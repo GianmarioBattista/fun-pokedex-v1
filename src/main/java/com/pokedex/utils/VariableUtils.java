5073c78389449451b13ea4d1bd21ea01ac1af870
GianmarioBattista <gianmario.battista@gmail.com>
2025-11-01 09:14:12 +0100

    feat: added VariableUtils classe for static strings

---
+++ src/main/java/com/pokedex/utils/VariableUtils.java
@@ -0,0 +1,22 @@
+package com.pokedex.utils;
+
+public class VariableUtils {
+
+    // Internal model map key definition
+    public final static String NAME = "name";
+    public final static String DESCRIPTION = "description";
+    public final static String HABITAT = "habitat";
+    public final static String IS_LEGENDARY = "isLegendary";
+
+    // Translation rest connector map key definition
+    public final static String CONTENTS = "contents";
+    public final static String TRANSLATED = "translated";
+
+    // Habitat types
+    public final static String CAVE = "cave";
+
+    // Translation types
+    public static final String YODA = "yoda";
+    public static final String SHAKESPEARE = "shakespeare";
+
+}
